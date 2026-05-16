14b60f6db6f68ccfa2e73e729aabb0dc8e530536
dddhena <dddhena62@gmail.com>
2026-05-16 13:49:41 -0700

    The modules and lessons management for the course

--- ElearningApplication/View/Instructor/ManageLessons.aspx.cs
+++ ElearningApplication/View/Instructor/ManageLessons.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +10,57 @@ namespace ElearningApplication.View.Instructor
     public partial class ManageLessons : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserId"] == null || Session["UserRole"]?.ToString() != "Instructor")
+            {
+                Response.Redirect("~/View/Account/Login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["courseId"] != null)
+                {
+                    ddlCourses.SelectedValue = Request.QueryString["courseId"];
+                }
+            }
+            Page.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ddlModules.SelectedValue))
+            {
+                Response.Write("<script>alert('Please select a module first.');</script>");
+                return;
+            }
+
+            try
+            {
+                sqlLessons.InsertParameters["Title"].DefaultValue = txtTitle.Text;
+                sqlLessons.InsertParameters["Content"].DefaultValue = txtContent.Text;
+                sqlLessons.InsertParameters["VideoUrl"].DefaultValue = txtVideoUrl.Text;
+                sqlLessons.InsertParameters["Duration"].DefaultValue = txtDuration.Text;
+                sqlLessons.InsertParameters["OrderNumber"].DefaultValue = txtOrder.Text;
+
+                sqlLessons.Insert();
+
+                // Clear fields
+                txtTitle.Text = "";
+                txtContent.Text = "";
+                txtVideoUrl.Text = "";
+                txtDuration.Text = "0";
+                txtOrder.Text = (int.Parse(txtOrder.Text) + 1).ToString();
+
+                Response.Write("<script>alert('Lesson added successfully!');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Error: " + ex.Message.Replace("'", "\\'") + "');</script>");
+            }
+        }
+
+        protected void gvLessons_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
--- ElearningApplication/View/Instructor/ManageModules.aspx.cs
+++ ElearningApplication/View/Instructor/ManageModules.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,51 @@ namespace ElearningApplication.View.Instructor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UserId"] == null || Session["UserRole"]?.ToString() != "Instructor")
+            {
+                Response.Redirect("~/View/Account/Login.aspx");
+                return;
+            }
 
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["courseId"] != null)
+                {
+                    ddlCourses.SelectedValue = Request.QueryString["courseId"];
+                }
+            }
+            Page.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ddlCourses.SelectedValue))
+            {
+                Response.Write("<script>alert('Please select a course first.');</script>");
+                return;
+            }
+
+            try
+            {
+                sqlModules.InsertParameters["Title"].DefaultValue = txtTitle.Text;
+                sqlModules.InsertParameters["Description"].DefaultValue = txtDescription.Text;
+                sqlModules.InsertParameters["OrderNumber"].DefaultValue = txtOrder.Text;
+                sqlModules.InsertParameters["IsLocked"].DefaultValue = chkIsLocked.Checked.ToString();
+
+                sqlModules.Insert();
+
+                // Clear fields
+                txtTitle.Text = "";
+                txtDescription.Text = "";
+                txtOrder.Text = (int.Parse(txtOrder.Text) + 1).ToString();
+                chkIsLocked.Checked = false;
+
+                Response.Write("<script>alert('Module added successfully!');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Error: " + ex.Message.Replace("'", "\\'") + "');</script>");
+            }
         }
     }
 }
